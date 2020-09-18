bcc513c2f98e8fa9f430e32fa8e7734d5e749bdf
Martin <ka_hei_martin_kwok@brown.edu>
2020-09-18 15:28:43 -0500

    Add flops calculation script

---
+++ get_flops.py
@@ -0,0 +1,56 @@
+import tensorflow as tf
+import os
+import numpy as np
+from tensorflow.keras.models import model_from_json
+from tensorflow.python.framework.convert_to_constants import convert_variables_to_constants_v2
+
+
+def get_flops_from_pb_v2(model_json):
+    with open(model_json,'r') as fjson:
+        model = model_from_json(fjson.read())
+        hdf5  = model_json.replace('json','hdf5')
+        model.load_weights(hdf5)
+        model.summary()
+        print(model)
+        inputs = [
+            tf.TensorSpec([1] + inp.shape[1:], inp.dtype) for inp in model.inputs
+        ]
+        full_model = tf.function(model).get_concrete_function(inputs)
+        frozen_func = convert_variables_to_constants_v2(full_model)
+        frozen_func.graph.as_graph_def()
+        layers = [op.name for op in frozen_func.graph.get_operations()]
+
+        # Calculate FLOPS with tf.profiler
+        run_meta = tf.compat.v1.RunMetadata()
+        opts = tf.compat.v1.profiler.ProfileOptionBuilder.float_operation()
+        flops = tf.compat.v1.profiler.profile(
+            graph=frozen_func.graph, run_meta=run_meta, cmd="scope", options=opts
+        )
+        return flops.total_float_ops
+ 
+if __name__=='__main__':
+
+    flist = [
+         './V11/signal/nElinks_5/Sep1_CNN_keras_norm/encoder_Sep1_CNN_keras_norm.json',
+         './V11/signal/nElinks_5/Sep1_CNN_keras_v12/encoder_Sep1_CNN_keras_v12.json',
+         './V11/signal/nElinks_5/Sep1_CNN_keras_v13/encoder_Sep1_CNN_keras_v13.json',
+         './V11/signal/nElinks_5/Sep1_CNN_keras_v14/encoder_Sep1_CNN_keras_v14.json',
+         './V11/signal/nElinks_5/Sep1_CNN_keras_v8/encoder_Sep1_CNN_keras_v8.json',
+         './V11/signal/nElinks_5/Sep1_CNN_keras_v9/encoder_Sep1_CNN_keras_v9.json',
+         './V11/signal/nElinks_5/Sep1_CNN_keras_v10/encoder_Sep1_CNN_keras_v10.json',
+         './V11/signal/nElinks_5/Sep1_CNN_keras_v1/encoder_Sep1_CNN_keras_v1.json',
+         './V11/signal/nElinks_5/Sep1_CNN_keras_v2/encoder_Sep1_CNN_keras_v2.json',
+         './V11/signal/nElinks_5/Sep1_CNN_keras_v3/encoder_Sep1_CNN_keras_v3.json',
+         './V11/signal/nElinks_5/Sep1_CNN_keras_v4/encoder_Sep1_CNN_keras_v4.json',
+         './V11/signal/nElinks_5/Sep1_CNN_keras_v7/encoder_Sep1_CNN_keras_v7.json',
+         './V11/signal/nElinks_5/Sep1_CNN_keras_v5/encoder_Sep1_CNN_keras_v5.json',
+         './V11/signal/nElinks_5/Sep1_CNN_keras_v6/encoder_Sep1_CNN_keras_v6.json',
+         './V11/signal/nElinks_5/Sep1_CNN_keras_v15/encoder_Sep1_CNN_keras_v15.json',
+         './V11/signal/nElinks_5/Sep1_CNN_keras_v16/encoder_Sep1_CNN_keras_v16.json'
+         ]
+    results = {}
+    for f in flist:
+        flops = get_flops_from_pb_v2(f)
+        results[f.split('/')[-1]]= flops
+    for k in results:
+        print(k,results[k])
