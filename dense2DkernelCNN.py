551c37387750fb32fdde93c957ec8082bc3e7277
Martin <ka_hei_martin_kwok@brown.edu>
2020-09-21 19:04:57 -0500

    generatlize dense2Dkernel

--- dense2DkernelCNN.py
+++ dense2DkernelCNN.py
@@ -12,10 +12,12 @@ class dense2DkernelCNN:
         self.pams ={
             'CNN_layer_nodes'  : [8],  #n_filters
             'CNN_kernel_size'  : [3],
+            'CNN_padding'      : ['same'],  
             'CNN_pool'         : [False],
+            'share_filters'    : False,
             'Dense_layer_nodes': [], #does not include encoded layer
             'encoded_dim'      : 12,
-            'shape'            : (1,4,4),
+            'shape'            : (4,4,3),
             'channels_first'   : False,
             'arrange'          : [],
             'arrMask'          : [],
@@ -80,26 +82,40 @@ class dense2DkernelCNN:
 
         CNN_layer_nodes   = self.pams['CNN_layer_nodes']
         CNN_kernel_size   = self.pams['CNN_kernel_size']
+        CNN_padding       = self.pams['CNN_padding']
         CNN_pool          = self.pams['CNN_pool']
         Dense_layer_nodes = self.pams['Dense_layer_nodes'] #does not include encoded layer
         channels_first    = self.pams['channels_first']
+        share_filters      = self.pams['share_filters']
 
         # fix to one cnn layer for now
-        nnodes=CNN_layer_nodes[0] #8
-        CNN_kernel=CNN_kernel_size[0] #3
+        nnodes     =CNN_layer_nodes[0] #8
+        CNN_kernel =CNN_kernel_size[0] #3
+        CNN_padding=CNN_padding[0]
 
         inputs = Input(shape=self.pams['shape'], name='input_1')
         x = inputs
         
-        x1 = Lambda(lambda x: x[:,:,:,0:1], output_shape=(4,4,1,), name='lambda_1')(x)
-        x2 = Lambda(lambda x: x[:,:,:,1:2], output_shape=(4,4,1,), name='lambda_2')(x)
-        x3 = Lambda(lambda x: x[:,:,:,2:3], output_shape=(4,4,1,), name='lambda_3')(x)
+        x1 = Lambda(lambda x: x[:,:,:,0:1], name='lambda_1')(x)
+        x2 = Lambda(lambda x: x[:,:,:,1:2], name='lambda_2')(x)
+        x3 = Lambda(lambda x: x[:,:,:,2:3], name='lambda_3')(x)
 
-        conv = Conv2D(nnodes, CNN_kernel, activation='relu', name='conv2d_1')
-        x1 = conv(x1)
-        x2 = conv(x2)
-        x3 = conv(x3)
+        if share_filters:
+            conv = Conv2D(nnodes, CNN_kernel, activation='relu',padding=CNN_padding, name='conv2d_1')
+            x1 = conv(x1)
+            x2 = conv(x2)
+            x3 = conv(x3)
+        else:
+            x1 = Conv2D(nnodes, CNN_kernel, activation='relu',padding=CNN_padding)(x1)
+            x2 = Conv2D(nnodes, CNN_kernel, activation='relu',padding=CNN_padding)(x2)
+            x3 = Conv2D(nnodes, CNN_kernel, activation='relu',padding=CNN_padding)(x3)
 
+        if CNN_pool[0]:
+           x1 = MaxPooling2D( (2,2), padding='same')(x1) 
+           x2 = MaxPooling2D( (2,2), padding='same')(x2) 
+           x3 = MaxPooling2D( (2,2), padding='same')(x3) 
+
+        conv_vol_slice = K.int_shape(x1)
         x1 = Flatten(name='flatten_1')(x1)
         x2 = Flatten(name='flatten_2')(x2)
         x3 = Flatten(name='flatten_3')(x3)
@@ -107,58 +123,62 @@ class dense2DkernelCNN:
         x = [x1,x2,x3]
         x = Concatenate(axis=-1,name='concat_1')(x)
 
+        conv_vol = K.int_shape(x)
+
         encodedLayer = Dense(encoded_dim, activation='relu',name='encoded_vector')(x)
 
         # Instantiate Encoder Model
         self.encoder = Model(inputs, encodedLayer, name='encoder')
         if printSummary:
           self.encoder.summary()
-          plot_model(self.encoder, show_shapes=True, to_file="model1.png")
 
         encoded_inputs = Input(shape=(encoded_dim,), name='decoder_input')
         x = encoded_inputs
 
-        x = Dense(96, activation='relu',name='dense_2')(x)
+        x = Dense(conv_vol[1], activation='relu',name='dense_2')(x)
 
-        x = Reshape((2,2,nnodes,3,),name='reshape_1')(x)
+        x = Reshape((conv_vol_slice[1],conv_vol_slice[2],nnodes,3,),name='reshape_1')(x)
         
-        x1 = Lambda(lambda x: x[:,:,:,:,0], output_shape=(2,2,nnodes,), name='lambda_4')(x)
-        x2 = Lambda(lambda x: x[:,:,:,:,1], output_shape=(2,2,nnodes,), name='lambda_5')(x)
-        x3 = Lambda(lambda x: x[:,:,:,:,2], output_shape=(2,2,nnodes,), name='lambda_6')(x)
+        x1 = Lambda(lambda x: x[:,:,:,:,0],  name='lambda_4')(x)
+        x2 = Lambda(lambda x: x[:,:,:,:,1],  name='lambda_5')(x)
+        x3 = Lambda(lambda x: x[:,:,:,:,2],  name='lambda_6')(x)
 
-        nchan=1
-        conv_t = Conv2DTranspose(nchan, CNN_kernel, activation=None, name='conv2d_transpose_1')
+        if CNN_pool[0]:
+           x1 = UpSampling2D( (2,2) )(x1) 
+           x2 = UpSampling2D( (2,2) )(x2) 
+           x3 = UpSampling2D( (2,2) )(x3) 
+
+        ## Always use 1 filter
+        conv_t = Conv2DTranspose(1, CNN_kernel, activation=None, padding=CNN_padding, name='conv2d_transpose_1')
         x1 = conv_t(x1)
         x2 = conv_t(x2)
         x3 = conv_t(x3)
 
         x = [x1,x2,x3]
         x = Concatenate(axis=-1,name='concat_2')(x)
-       
+
         outputs = Activation('sigmoid', name='decoder_output')(x)
 
         self.decoder = Model(encoded_inputs, outputs, name='decoder')
         if printSummary:
           self.decoder.summary()
-          plot_model(self.decoder, show_shapes=True, to_file="model2.png")
 
         self.autoencoder = Model(inputs, self.decoder(self.encoder(inputs)), name='autoencoder')
         if printSummary:
           self.autoencoder.summary()
-          plot_model(self.autoencoder, show_shapes=True, to_file="model.png")
 
-        # if self.pams['loss']=="weightedMSE":
-        #     self.autoencoder.compile(loss=self.weightedMSE, optimizer='adam')
-        #     self.encoder.compile(loss=self.weightedMSE, optimizer='adam')
-        # elif self.pams['loss'] == 'telescopeMSE':
-        self.autoencoder.compile(loss=telescopeMSE2, optimizer='adam', run_eagerly=True)
-        self.encoder.compile(loss=telescopeMSE2, optimizer='adam', run_eagerly=True)
-        # elif self.pams['loss']!='':
-        #     self.autoencoder.compile(loss=self.pams['loss'], optimizer='adam')
-        #     self.encoder.compile(loss=self.pams['loss'], optimizer='adam')
-        # else:
-        #     self.autoencoder.compile(loss='mse', optimizer='adam')
-        #     self.encoder.compile(loss='mse', optimizer='adam')
+        if self.pams['loss']=="weightedMSE":
+            self.autoencoder.compile(loss=self.weightedMSE, optimizer='adam')
+            self.encoder.compile(loss=self.weightedMSE, optimizer='adam')
+        elif self.pams['loss'] == 'telescopeMSE':
+            self.autoencoder.compile(loss=telescopeMSE2, optimizer='adam', run_eagerly=True)
+            self.encoder.compile(loss=telescopeMSE2, optimizer='adam', run_eagerly=True)
+        elif self.pams['loss']!='':
+            self.autoencoder.compile(loss=self.pams['loss'], optimizer='adam')
+            self.encoder.compile(loss=self.pams['loss'], optimizer='adam')
+        else:
+            self.autoencoder.compile(loss='mse', optimizer='adam')
+            self.encoder.compile(loss='mse', optimizer='adam')
 
         CNN_layers=''
         if len(CNN_layer_nodes)>0:
