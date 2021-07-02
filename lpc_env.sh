4905483f1e7d4eecc07b7df39c5331a50ff773d2
rohanshenoy <42732515+rohanshenoy@users.noreply.github.com>
2021-07-02 16:48:52 +0530

    Delete lpc_env.sh

--- lpc_env.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-NAME=hgcalKeras
-LCG=/cvmfs/sft.cern.ch/lcg/views/LCG_96python3/x86_64-centos7-gcc8-opt/setup.sh
-
-source $LCG
-python -m venv --copies $NAME
-source $NAME/bin/activate
-export PYTHONPATH=$PWD/$NAME/lib/python3.6/site-packages:$PYTHONPATH
